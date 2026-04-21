469a42a4c742a45d61d24202356f0fcf092e0f52
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-04-21 18:54:19 +0200

    add and register post type

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import { categoryType } from './categoryType'
 import { authorType } from './authorType'
+import { postType } from './postType'
 
-export const schemaTypes = [categoryType, authorType]
+export const schemaTypes = [categoryType, authorType, postType]
---
+++ schemaTypes/postType.ts
@@ -0,0 +1,42 @@
+import { defineField, defineType } from 'sanity'
+
+export const postType = defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+    }),
+    defineField({
+      name: 'author',
+      type: 'reference',
+      to: [{ type: 'category '}],
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'mainImage',
+      type: 'image'
+    }),
+    defineField({
+      name: 'excerpt',
+      type: 'text',
+    }),
+    defineField({
+      name: 'body',
+      type: 'array',
+      of: [{ type: 'block '}]
+    }),
+  ],
+})
